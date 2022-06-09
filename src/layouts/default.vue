0ceedf1897842657f044ecc163c9a5752d463dac
ruben.ruiz <rubenruizpedreira@gmail.com>
2022-06-09 23:56:42 +0200

    fithub to icon

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -29,14 +29,14 @@ const toggleLocales = () => {
       <div flex flex-row gap-4>
         <a href="/blog" :title="t('button.blog')">{{ t('button.blog') }}</a>
         <a href="/about" :title="t('button.about')">{{ t('button.about') }}</a>
-        <a rel="noreferrer" href="https://github.com/erxonxi" target="_blank">
-          GitHub
-        </a>
       </div>
     </div>
     <div class="basis-2/4 text-right text-lg mt-2">
       <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales()">
         <div i-carbon-language />
+      </a>
+      <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/erxonxi" target="_blank" title="GitHub">
+        <div i-carbon-logo-github />
       </a>
       <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
         <div i="carbon-sun dark:carbon-moon" />
