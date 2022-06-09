1dcfa26d26933598e353abf54245c626e5c33fc5
ruben.ruiz <rubenruizpedreira@gmail.com>
2022-06-09 16:17:30 +0200

    post index

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
 const { t } = useI18n()
+const lastPosts = ref([
+  {
+    slug: 'first-post',
+    title: 'Mi Primer Post',
+  },
+])
 </script>
 
 <template>
   <div text-left container mx-auto max-w-2xl my-4>
-    <p text-2xl font-bold mb-2 underline underline-offset-2>
+    <h1 text-2xl font-bold mb-2 underline underline-offset-2>
       {{ t('blog.last_posts') }}
-    </p>
-    <p text-center font-bold>
-      {{ t('not-found') }}
-    </p>
+    </h1>
+
+    <div grid gap-4 my-6>
+      <a v-for="post in lastPosts" :key="post.slug" drop-shadow-xl border-4 border-gray-500 p-4 rounded-xl :href="`/blog/${post.slug}`">
+        <h2>{{ post.title }}</h2>
+      </a>
+    </div>
   </div>
 </template>
 
