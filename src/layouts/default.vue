76f61006e7310dbb5016f8ccc1b1c9f1e4a97b36
ruben.ruiz <rubenruizpedreira@gmail.com>
2022-06-09 16:04:33 +0200

    fixing font size

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,7 +3,7 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
+  <main class="text-lg px-4 py-10 text-center text-gray-700 dark:text-gray-200">
     <div text-4xl>
       <div i-carbon-campsite inline-block />
     </div>
