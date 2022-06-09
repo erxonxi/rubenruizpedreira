b08a5cbb34c46cc8b759ade028a61402d13c3bec
ruben.ruiz <rubenruizpedreira@gmail.com>
2022-06-09 14:52:06 +0200

    blog

---
+++ src/pages/blog/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
+<template>
+  <div text-left container mx-auto max-w-2xl my-4>
+    <p text-2xl font-bold mb-2 underline underline-offset-2>
+      {{ t('blog.last_posts') }}
+    </p>
+    <p text-center font-bold>
+      {{ t('not-found') }}
+    </p>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: default
+</route>
+
