631a95e84e1f7847bf6e1181f09eee1996145ed4
ruben.ruiz <rubenruizpedreira@gmail.com>
2022-06-11 00:24:22 +0200

    fixing all

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,5 +2,11 @@
   <footer grid gap-4 my-8>
     <em text-sm opacity-75>2022 by Rubén Ruiz Pedreira</em>
     <NewslatterModal />
+    <ins
+      class="adsbygoogle"
+      style="display:inline-block;width:728px;height:90px"
+      data-ad-client="ca-pub-1234567890123456"
+      data-ad-slot="1234567890"
+    />
   </footer>
 </template>
