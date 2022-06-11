8a80bdadf83034426cabb4d885cf8e8010ed0714
ruben.ruiz <rubenruizpedreira@gmail.com>
2022-06-11 14:53:01 +0200

    navbar size fixing

--- src/components/NavBarMenu.vue
+++ src/components/NavBarMenu.vue
@@ -23,7 +23,7 @@ const toggleLocales = () => {
 <template>
   <Menu as="div" class="relative inline-block text-left">
     <div>
-      <MenuButton title="Menu" text-2xl inline-flex i-carbon-overflow-menu-horizontal />
+      <MenuButton title="Menu" text-4xl inline-flex i-carbon-overflow-menu-horizontal />
     </div>
 
     <transition
@@ -34,11 +34,11 @@ const toggleLocales = () => {
       leave-from-class="transform scale-100 opacity-100"
       leave-to-class="transform scale-95 opacity-0"
     >
-      <MenuItems absolute z-10 right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-xs bg-white dark:bg-neutral-900 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none>
-        <div class="px-1 py-1">
+      <MenuItems absolute z-10 right-0 mt-1 w-64 origin-top-right divide-y divide-gray-100 rounded-xs bg-white dark:bg-neutral-900 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none>
+        <div class="px-1 py-1 text-xl">
           <MenuItem v-slot="{ active }">
             <a
-              href="/blog" class="group flex w-full items-center rounded-sm px-2 py-2" :class="[
+              href="/blog" class="group flex w-full items-center rounded-sm p-4" :class="[
                 active ? 'bg-gray-500 dark:bg-gray-500 text-white' : 'text-gray-800 dark:text-gray-200',
               ]" :title="t('button.blog')"
             >
@@ -47,7 +47,7 @@ const toggleLocales = () => {
           </MenuItem>
           <MenuItem v-slot="{ active }">
             <a
-              href="/projects" class="group flex w-full items-center rounded-sm px-2 py-2" :class="[
+              href="/projects" class="group flex w-full items-center rounded-sm p-4" :class="[
                 active ? 'bg-gray-500 dark:bg-gray-500 text-white' : 'text-gray-800 dark:text-gray-200',
               ]" :title="t('button.projects')"
             >
@@ -56,7 +56,7 @@ const toggleLocales = () => {
           </MenuItem>
           <MenuItem v-slot="{ active }">
             <a
-              href="/about" class="group flex w-full items-center rounded-sm px-2 py-2" :class="[
+              href="/about" class="group flex w-full items-center rounded-sm p-4" :class="[
                 active ? 'bg-gray-500 dark:bg-gray-500 text-white' : 'text-gray-800 dark:text-gray-200',
               ]" :title="t('button.about')"
             >
@@ -64,7 +64,7 @@ const toggleLocales = () => {
             </a>
           </MenuItem>
           <MenuItem>
-            <div my-4 grid grid-cols-3 text-xl>
+            <div my-4 grid grid-cols-3 text-2xl>
               <a class="icon-btn mx-auto" :title="t('button.toggle_langs')" @click="toggleLocales()">
                 <div i-carbon-language />
               </a>
