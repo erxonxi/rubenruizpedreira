2668eb431cf4094b5d81851715e1ee80fcc17c66
ruben.ruiz <rubenruizpedreira@gmail.com>
2022-06-12 14:11:23 +0200

    Revert "try this to production"
    This reverts commit 2c3cd5929a505a76b1a80668589846f300db0e85.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
   <footer grid gap-4 my-8>
     <em text-sm opacity-75>2022 by Rubén Ruiz Pedreira</em>
-    <NewslatterModal />
+    <!-- <NewslatterModal /> -->
     <ins
-      class="adsbygoogle w-full max-w-xl"
+      class="adsbygoogle w-full"
       style="display:inline-block;height:90px"
       data-ad-client="ca-pub-1234567890123456"
       data-ad-slot="1234567890"
