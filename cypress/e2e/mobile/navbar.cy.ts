bd195848f03501003159a1ce9ac2eb5bef9eab0d
ruben.ruiz <rubenruizpedreira@gmail.com>
2022-06-30 22:50:04 +0200

    tests: adding more tests navbar

--- cypress/e2e/mobile/navbar.cy.ts
+++ cypress/e2e/mobile/navbar.cy.ts
@@ -1,4 +1,4 @@
-context('GIVEN the user is on the mobile navbar', () => {
+context('GIVEN the user is in home and need open menu', () => {
   beforeEach(() => {
     cy.viewport('samsung-s10');
     cy.visit('/');
@@ -15,6 +15,10 @@ context('GIVEN the user is on the mobile navbar', () => {
 
     it('AND the blog link should be visible', () => {
       cy.get('#menu-nav-bar > [title="Blog"]').should('be.visible');
+    });
+
+    it('AND the projects link should be visible', () => {
+      cy.get('#menu-nav-bar > [title="Projects"]').should('be.visible');
     });
 
     it('AND the services link should be visible', () => {
@@ -38,3 +42,79 @@ context('GIVEN the user is on the mobile navbar', () => {
     });
   });
 });
+
+context('GIVEN the user is on the mobile navbar', () => {
+  beforeEach(() => {
+    cy.viewport('samsung-s10');
+    cy.visit('/');
+
+    cy.get('[title="Menu"]')
+      .click();
+  });
+
+  context('WHEN the blog link is clicked', () => {
+    beforeEach(() => {
+      cy.get('#menu-nav-bar > [title="Blog"]').click();
+    });
+
+    it('THEN redirect user to /blog', () => {
+      cy.url().should(
+        'include',
+        '/blog'
+      );
+    });
+  });
+
+  context('WHEN the projects link is clicked', () => {
+    beforeEach(() => {
+      cy.get('#menu-nav-bar > [title="Projects"]').click();
+    });
+
+    it('THEN redirect user to /projects', () => {
+      cy.url().should(
+        'include',
+        '/projects'
+      );
+    });
+  });
+
+  context('WHEN the services link is clicked', () => {
+    beforeEach(() => {
+      cy.get('#menu-nav-bar > [title="Services"]').click();
+    });
+
+    it('THEN redirect user to /services', () => {
+      cy.url().should(
+        'include',
+        '/services'
+      );
+    });
+  });
+
+  context('WHEN the about link is clicked', () => {
+    beforeEach(() => {
+      cy.get('#menu-nav-bar > [title="About"]').click();
+    });
+
+    it('THEN redirect user to /about', () => {
+      cy.url().should(
+        'include',
+        '/about'
+      );
+    });
+  });
+
+  context('WHEN the change languages button is clicked', () => {
+    beforeEach(() => {
+      cy.get('[title="Change languages"]')
+        .last()
+        .click();
+    });
+
+    it('THEN the web change lenguage', () => {
+      cy.contains('Trabajo')
+        .should('exist');
+    });
+  });
+});
+
